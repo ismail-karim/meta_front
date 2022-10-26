b6004232d43a73518bc06e49e2999a94f79c2eb0
ismail KARIMALLAH <ikarimallah@gmail.com>
2022-10-26 12:33:55 +0200

    Creating a homepage and header / footer component
    Creating the structure base of homepage application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'home', loadChildren: () => import('./homepage/homepage.module').then((m) => m.HomepageModule)},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path:'**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
